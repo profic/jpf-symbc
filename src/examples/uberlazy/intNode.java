0f4b8d6881bdfab68b891d19dcbca249e2b25d55
pcorina <pcorina@localhost>
2010-02-16 17:32:37 -0800

    fixed JPF_java_lang_String -- still buggy updated MinMax with larger values

---
+++ src/examples/uberlazy/intNode.java
@@ -0,0 +1,14 @@
+package uberlazy;
+
+public class intNode extends Node {
+	int elem;
+	Node next;
+	
+	public intNode() {
+		
+	}
+	
+	public void print() {
+		System.out.println("I am an intNode");
+	}
+}
